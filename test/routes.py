1cf55f8fbcf049c0706c4cf946b5e1d06b24b06a
Сокорьян Иван Сергеевич <sokoryan_39808@hq.bc>
2019-01-17 14:57:50 +0600

    test

---
+++ test/routes.py
@@ -0,0 +1,22 @@
+from flask import render_template
+from test import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beatiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
