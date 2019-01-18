a052d978388c260f3ff8c7c70117a55e51034bb3
demch1k <demch1k.mid@gmail.com>
2019-01-18 14:35:21 +0600

    test1

--- microblog.py
+++ microblog.py
@@ -1,1 +1,6 @@
-from test import app
+from test import app, db
+from test.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
